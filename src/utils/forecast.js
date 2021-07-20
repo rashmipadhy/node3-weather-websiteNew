f74846a824fae2957182e2c68e6f6be7ce45797e
Rashmi <rashmipadhy.mc@gmail.com>
2021-07-20 15:14:04 +0530

    Add high temperature to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -27,7 +27,8 @@ const forecast = (latitude,longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location. Try another search.', undefined)
         } else {
-            callback(undefined,body.current.weather_descriptions + '. It is currently '+body.current.temperature +' degree out. There is a ' + body.current.precip + '% chance of rain.')         
+            //console.log(body.current.weather_descriptions[0])
+            callback(undefined,body.current.weather_descriptions[0] + '. It is currently '+body.current.temperature +' degree out. It fells like  ' + body. current.feelslike +' degree out. The humidity is '+body.current.humidity + '%' + ' There is a ' + body.current.precip + '% chance of rain.')         
             
         }
     })
